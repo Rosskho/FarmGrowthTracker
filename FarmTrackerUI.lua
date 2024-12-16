54abb61fb57b6c03ee6eb1188ed7dd293e4fc306
Rosskho <139445890+Rosskho@users.noreply.github.com>
2024-12-15 18:05:16 -0800

    Update FarmTrackerUI.lua
    added the hotkey script

--- FarmTrackerUI.lua
+++ FarmTrackerUI.lua
@@ -1,11 +1,13 @@
 FarmTrackerUI = ISPanel:derive("FarmTrackerUI")
 
+-- Initialize the FarmTrackerUI
 function FarmTrackerUI:initialise()
     ISPanel.initialise(self)
     self.title = "Farm Growth Tracker"
     self.crops = FarmTrackerUtils.getCropData()
 end
 
+-- Render the UI elements
 function FarmTrackerUI:render()
     self:drawText(self.title, 10, 10, 1, 1, 1, 1, UIFont.Medium)
     local y = 40
@@ -19,6 +21,7 @@ function FarmTrackerUI:render()
     end
 end
 
+-- Create a new instance of the UI
 function FarmTrackerUI:new(x, y, width, height)
     local o = ISPanel.new(self, x, y, width, height)
     o:setX(x)
@@ -28,3 +31,15 @@ function FarmTrackerUI:new(x, y, width, height)
     o:addToUIManager()
     return o
 end
+
+-- Hotkey function to toggle the tracker UI
+Events.OnKeyPressed.Add(function(key)
+    if key == Keyboard.KEY_T then -- Replace KEY_T with your desired hotkey
+        if not FarmTrackerUI.instance then
+            FarmTrackerUI.instance = FarmTrackerUI:new(200, 200, 400, 300)
+        else
+            FarmTrackerUI.instance:removeFromUIManager()
+            FarmTrackerUI.instance = nil
+        end
+    end
+end)
